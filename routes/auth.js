c095a273588a79d66957ab33ac11c0285299e3bb
najib iziki <avrnikh@gmail.com>
2025-07-15 20:05:03 +0100

    test

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../controller/auth')
+const auth = require("../controller/auth");
+console.log("test");
+router.post("/", auth.signup);
+router.post("/token", auth.token);
+router.post("/token/refresh", auth.token_refresh);
 
-router.post('/',auth.signup)
-router.post('/token', auth.token)
-router.post('/token/refresh', auth.token_refresh)
-
-
-module.exports = router
+module.exports = router;
